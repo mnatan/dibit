acdd4573e29347ed92bd7c10cc86368ea35099e4
Marcin Natanek <Marcin.Natanek@dreamlab.pl>
2017-05-13 22:52:14 +0200

    #2 Login and register in GQL

--- server/utils/schema.js
+++ server/utils/schema.js
@@ -28,6 +28,18 @@ exports.schema = new GraphQLSchema({
                     password: {type: new GraphQLNonNull(GraphQLString)},
                 },
                 resolve: (obj, args, ctx) => auth.generateToken(args.username, args.password),
+            },
+            register: {
+                type: User.Type,
+                description: 'Used to authenticate the user',
+                args: {
+                    username: {type: new GraphQLNonNull(GraphQLString)},
+                    password: {type: new GraphQLNonNull(GraphQLString)},
+                    firstName: {type: new GraphQLNonNull(GraphQLString)},
+                    lastName: {type: new GraphQLNonNull(GraphQLString)},
+                    dateOfBirth: {type: new GraphQLNonNull(GraphQLString), description: "format: 'YYYY-MM-DD'"},
+                },
+                resolve: (obj, args, ctx) => auth.createUser(args, args.password),
             }
         }
     })
